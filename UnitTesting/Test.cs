9ae903feb4b0cc2b7ba3d41ff921d2ffe40ec3fc
annajerofejeva <jerofejeva.anna@yahoo.com>
2014-12-18 01:51:54 +0000

    Added Divisible by three method and test

---
+++ UnitTesting/Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using fizzbuzzcsharp;
+
+namespace UnitTesting
+{
+	[TestFixture ()]
+	public class FizzbuzzTests
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			Fizzbuzz game = new Fizzbuzz();
+			Assert.AreEqual(true, game.DivisibleByThree(3));
+		}
+	}
+}
+
---
+++ fizzbuzz-csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fizzbuzzcsharp
+{
+	public class Fizzbuzz
+	{
+		public static void Main ()
+		{
+			Console.WriteLine ("Hello World!");
+		}
+
+		public bool DivisibleByThree(int number)
+		{
+			return number % 3 == 0;
+		}
+	}
+}
